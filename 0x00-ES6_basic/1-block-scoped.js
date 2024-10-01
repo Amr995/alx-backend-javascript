7d7521ef2669726623ab9520006bc8da2f02b68e
Amr995 <amr995@yahoo.com>
2024-10-01 06:18:38 +0000

    1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,8 +4,9 @@ export default function taskBlock(trueOrFalse) {
 
 	/* eslint-disable no-unused-vars */
 	if (trueOrFalse) {
-		let task = true;
-		let task2 = false;
+		const task = true;
+		const task2 = false;
+		return [task, task2];
 	}
 	/* eslint-enable no-unused-vars */
 
