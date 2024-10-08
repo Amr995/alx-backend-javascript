8decb3a0879e643bad6eb98c89392d4ce51801ec
Amr995 <amr995@yahoo.com>
2024-10-08 18:56:27 +0000

    Readme & 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+getListStudents() => {
+	const Arr = [
+		{
+			const firstName: "Guillaume",
+			const id: 1,
+			const location: "San Francisco":
+		},
+		{
+			const firstName: "Guillaume",
+			const id: 1,
+			const location: "San Francisco":
+		},
+		{
+			const firstName: "Guillaume",
+			const id: 1,
+			const location: "San Francisco":
+		}
+	]
+	return ...Arr;
+}
